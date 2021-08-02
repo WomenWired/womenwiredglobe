755e5819d27579dd6b5fe5105754f2f318e9f90e
Kaya Jones <kayacancode@gmail.com>
2021-08-02 14:47:30 -0500

    Updates

---
+++ precache-manifest.ab4e42f5c59c26e06442d743de37aea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d49a96f64fc926be468f529af65ffaf",
+    "url": "/womenwiredglobe/index.html"
+  },
+  {
+    "revision": "a5c50a8703eb48d2105c",
+    "url": "/womenwiredglobe/static/css/2.61a570a4.chunk.css"
+  },
+  {
+    "revision": "db82e4efa2da5c9d735a",
+    "url": "/womenwiredglobe/static/css/main.890b0b59.chunk.css"
+  },
+  {
+    "revision": "a5c50a8703eb48d2105c",
+    "url": "/womenwiredglobe/static/js/2.7df0c7d0.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/womenwiredglobe/static/js/2.7df0c7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db82e4efa2da5c9d735a",
+    "url": "/womenwiredglobe/static/js/main.4fe219d4.chunk.js"
+  },
+  {
+    "revision": "e39599b09ea94091ee50",
+    "url": "/womenwiredglobe/static/js/runtime-main.35a84b28.js"
+  }
+]);
